8ebbf0b247c0fea3524ac6e29e7541ced1c09e8b
sherwin-cell <sherwinescanilla18@gmail.com>
2026-05-14 03:19:09 +0800

    Add Google OAuth login

---
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleController extends Controller
+{
+    // Redirect to Google
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    // Handle callback
+    public function callback()
+    {
+        $googleUser = Socialite::driver('google')->user();
+
+        $user = User::updateOrCreate(
+            ['email' => $googleUser->getEmail()],
+            [
+                'name'              => $googleUser->getName(),
+                'google_id'         => $googleUser->getId(),
+                'avatar'            => $googleUser->getAvatar(),
+                'password'          => bcrypt(str()->random(24)),
+                'email_verified_at' => now(),
+            ]
+        );
+
+        Auth::login($user);
+
+        return redirect()->intended('/dashboard');
+    }
+}
